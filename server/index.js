6e268c9a87326a8cbb3216a5ca5ab463d85161f6
Samuel <kungusamuel64@gmail.com>
2022-02-17 20:42:35 +0300

    Recent Updates

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.json());
 
-app.listen("3005", ()=>{
-    console.log("Server is runing on port 3005...")
+const server = app.listen("3002", () => {
+  console.log("Server Running on Port 3002...");
+});
+
+io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log("User Joined Room: " + data);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data.content);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("USER DISCONNECTED");
+  });
 });
